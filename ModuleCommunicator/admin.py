8472e8282962215bf7882eddf44a0065e41d99e4
JinhaSong <thejn07@gmail.com>
2019-10-28 03:18:43 +0900

    crack classification + segmentation sequential processing 부분을 communicator에 삽입 - TODO   * module name 제거   * connected component 함수(또는 class) 추가

--- ModuleCommunicator/admin.py
+++ ModuleCommunicator/admin.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import ImageModel, ResultModel
+
+admin.site.register(ImageModel)
+admin.site.register(ResultModel)
+
 # Register your models here.
--- ModuleCommunicator/tasks.py
+++ ModuleCommunicator/tasks.py
@@ -3,18 +3,36 @@ from __future__ import print_function
 from AnalysisSite.celerys import app
 import json
 import requests
+import ast
+import time
 
 
 @app.task
 def communicator(url, image_path):
-    json_data = dict()
-    json_image = open(image_path, 'rb')
-    json_files = {'image': json_image}
+    cls_data = dict()
+    cls_image = open(image_path, 'rb')
+    cls_files = {'image': cls_image}
 
-    result_response = requests.post(url=url, data=json_data, files=json_files)
-    result_data = json.loads(result_response.content)
-    result = result_data['result']
+    start = time.time()
+    cls_response = requests.post(url=url, data=cls_data, files=cls_files)
+    result_data = json.loads((cls_response.content).decode('utf-8'))
+    end = time.time()
+    print("====== classification fin {} ======".format(end-start))
 
-    json_image.close()
+    start = time.time()
+    seg_data = dict()
+    seg_file = json.dumps(result_data, ensure_ascii=False, indent='')
+    seg_files = {'file': seg_file}
+    seg_response = requests.post(url='http://mltwins.sogang.ac.kr:8002', data=seg_data, files=seg_files)
+    end = time.time()
+    print("====== segmentation fin {} ======".format(end-start))
+
+    seg_result_data = json.loads((seg_response.content).decode('utf-8'))
+    seg_image = seg_result_data['result'][0]['label'][0]['description']
+    result = {}
+    result['classification_result'] = result_data['result']
+    result['image'] = seg_image
+
+    cls_image.close()
 
     return result
