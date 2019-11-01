d5d50454f211312fc32dbfb627d645d978d9e718
JinhaSong <thejn07@gmail.com>
2019-11-01 16:23:16 +0900

    LC TC AC classification model 연동 및 결과 json에 추가

--- ModuleCommunicator/tasks.py
+++ ModuleCommunicator/tasks.py
@@ -9,19 +9,18 @@ import time
 
 @app.task
 def communicator(url, image_path):
+    start = time.time()
     cls_data = dict()
     cls_image = open(image_path, 'rb')
     cls_files = {'image': cls_image}
-
-    start = time.time()
     cls_response = requests.post(url=url, data=cls_data, files=cls_files)
-    result_data = json.loads((cls_response.content).decode('utf-8'))
+    cls_result_data = json.loads((cls_response.content).decode('utf-8'))
     end = time.time()
     print("====== classification fin {} ======".format(end-start))
 
     start = time.time()
     seg_data = dict()
-    seg_file = json.dumps(result_data, ensure_ascii=False, indent='')
+    seg_file = json.dumps(cls_result_data, ensure_ascii=False, indent='')
     seg_files = {'file': seg_file}
     seg_response = requests.post(url='http://mltwins.sogang.ac.kr:8002', data=seg_data, files=seg_files)
     end = time.time()
@@ -29,10 +28,28 @@ def communicator(url, image_path):
 
     seg_result_data = json.loads((seg_response.content).decode('utf-8'))
     seg_image = seg_result_data['result'][0]['label'][0]['description']
-    result = {}
-    result['classification_result'] = result_data['result']
-    result['image'] = seg_image
+
+    start = time.time()
+    cls_detail_data = dict()
+    cls_detail_data['image'] = seg_image
+    cls_detail_response = requests.post(url='http://mltwins.sogang.ac.kr:8003', data=cls_detail_data)
+    cls_detail_result_data = json.loads((cls_detail_response.content).decode('utf-8'))
+    end = time.time()
+    print("====== classification detail fin {} ======".format(end-start))
+
+    for i in range(len(cls_result_data['result'])) :
+        cls_position = cls_result_data['result'][i]['position']
+        for j in range(len(cls_detail_result_data['result'])) :
+            cls_detail_position = cls_detail_result_data['result'][j]['position']
+            if cls_position['x'] == cls_detail_position['x'] and cls_position['y'] == cls_detail_position['y'] :
+                cls_result_data['result'][i]['label'].extend(cls_detail_result_data['result'][j]['label'])
+                j = len(cls_detail_result_data['result'])
+                print(i, j, cls_position['x'], cls_detail_position['x'], cls_position['y'], cls_detail_position['y'])
 
     cls_image.close()
+
+    result = {}
+    result['classification_result'] = cls_result_data['result']
+    result['image'] = seg_image
 
     return result
